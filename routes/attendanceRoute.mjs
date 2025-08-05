f6a31981f7fca78cfb83f7d73fdd19e30947993d
darkshade <darkshade@kali.DarkShade>
2025-08-05 13:25:55 +0100

    updated courses and workflow

--- routes/attendanceRoute.mjs
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { markAttendance } from '../controllers/attendanceControllers.mjs';
-
-const attendanceRouter = Router();
-attendanceRouter.post('/mark', markAttendance);
-
-export default attendanceRouter;
